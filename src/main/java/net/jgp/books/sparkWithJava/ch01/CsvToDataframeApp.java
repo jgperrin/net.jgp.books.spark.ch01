4c3570a27b5ea27c2c74fd08feaa4200cf9eb4e9
jgp <jgp@10.0.100.139>
2018-03-12 17:03:19 -0400

    Typo

--- src/main/java/net/jgp/books/sparkWithJava/ch01/CsvToDataframeApp.java
+++ src/main/java/net/jgp/books/sparkWithJava/ch01/CsvToDataframeApp.java
@@ -7,35 +7,36 @@ import org.apache.spark.sql.SparkSession;
 /**
  * CSV ingestion in a dataframe.
  * 
- * @author jperrin
+ * @author jgp
  */
 public class CsvToDataframeApp {
 
-	/**
-	 * main() is your entry point to the application. 
-	 * @param args
-	 */
-    public static void main(String[] args) {
-        CsvToDataframeApp app = new CsvToDataframeApp();
-        app.start();
-    }
+  /**
+   * main() is your entry point to the application.
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    CsvToDataframeApp app = new CsvToDataframeApp();
+    app.start();
+  }
 
-    /**
-     * The processing code.
-     */
-    private void start() {
-        // Creates a session on a local master
-        SparkSession spark = SparkSession.builder()
-                .appName("CSV to Dataset")
-                .master("local")
-                .getOrCreate();
+  /**
+   * The processing code.
+   */
+  private void start() {
+    // Creates a session on a local master
+    SparkSession spark = SparkSession.builder()
+        .appName("CSV to Dataset")
+        .master("local")
+        .getOrCreate();
 
-        // Reads a CSV file with header, called books.csv, stores it in a dataframe
-        Dataset<Row> df = spark.read().format("csv")
-                .option("header", "true")
-                .load("data/books.csv");
+    // Reads a CSV file with header, called books.csv, stores it in a dataframe
+    Dataset<Row> df = spark.read().format("csv")
+        .option("header", "true")
+        .load("data/books.csv");
 
-        // Shows at most 5 rows from the dataframe
-        df.show(5);
-    }
+    // Shows at most 5 rows from the dataframe
+    df.show(5);
+  }
 }
